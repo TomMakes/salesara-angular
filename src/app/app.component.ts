1b817902946ff1aa4b8ea9bc096a173f4a11d174
Thomas Ryszkiewicz <tom.ryszkiewicz@gmail.com>
2024-09-14 20:54:45 -0400

    Added type-ahead to app component. Fixed test for salesara title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SsaTypeAheadComponent } from "./core-ui/ssa-type-ahead/ssa-type-ahead.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SsaTypeAheadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'salesara-angular';
+  testOptions = [
+    { name: 'first option', value: '1' },
+    { name: 'second option', value: '2' }
+  ];
 }
