2f0883e2702f06eb8a457ce680cafe0c4c6d9bda
Thomas Ryszkiewicz <tom.ryszkiewicz@gmail.com>
2024-09-14 20:55:15 -0400

    Added tests: Test to check if given list of options are rendered as mat-options. Test to check if typing certain words into the input displays certain options and not others.

--- src/app/core-ui/ssa-type-ahead/ssa-type-ahead.component.spec.ts
+++ src/app/core-ui/ssa-type-ahead/ssa-type-ahead.component.spec.ts
@@ -1,12 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { SsaTypeAheadComponent } from './ssa-type-ahead.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('SsaTypeAheadComponent', () => {
   let component: SsaTypeAheadComponent;
   let fixture: ComponentFixture<SsaTypeAheadComponent>;
-  let compEl: HTMLElement ;
+  let compEl: HTMLElement;
+  let compDe: DebugElement;
+  const testOptions = [
+    { name: 'first option', value: '1' },
+    { name: 'second option', value: '2' }
+  ];
+
+  const QUARTER_SECOND = 250;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,6 +26,9 @@ describe('SsaTypeAheadComponent', () => {
     fixture = TestBed.createComponent(SsaTypeAheadComponent);
     component = fixture.componentInstance;
     compEl = fixture.nativeElement;
+    compDe = fixture.debugElement;
+    component.autoCompleteOptions = testOptions;
+    // This calls ngOnInit
     fixture.detectChanges();
   });
 
@@ -26,14 +38,48 @@ describe('SsaTypeAheadComponent', () => {
   // Tests to check if a mat-form-field, input, and mat-autocomplete are rendered.
   it('should contain a mat-form-field', () => {
     const formFieldEl = compEl.querySelector('mat-form-field');
-    expect(formFieldEl).toBeDefined();
+    expect(formFieldEl).toBeTruthy();
   });
   it('should contain a input', () => {
     const inputEl = compEl.querySelector('input');
-    expect(inputEl).toBeDefined();
+    expect(inputEl).toBeTruthy();
   });
   it('should contain a mat-autocomplete', () => {
     const autocompleteEl = compEl.querySelector('mat-autocomplete');
-    expect(autocompleteEl).toBeDefined();
+    expect(autocompleteEl).toBeTruthy();
   });
+  // Test to check if given list of options are rendered as mat-options.
+  it('should render given options in autocomplete when selected', () => {
+    const inputDe: DebugElement = compDe.query(By.css('input'));
+    inputDe.triggerEventHandler('click');
+    fixture.detectChanges();
+
+    for ( const opt of testOptions ) {
+      // Here I tried using querySelector on component native element, but it seems like the native element does not update.
+      // Maybe my test suite isn't rendering in a browser?
+      const typeAheadOption = compDe.query( By.css( `mat-option[qa-tag="type-ahead-option-${opt.name}"]` ) );
+      expect( typeAheadOption ).toBeTruthy();
+    }
+  });
+  // Test to check if typing certain words into the input displays certain options and not others.
+  it('should render certain options in autocomplete when typing certain words', fakeAsync( () => {
+    // Adding this fixes error "Error: 1 periodic timer(s) still in the queue."
+    fixture.autoDetectChanges();
+
+    const inputEl: HTMLInputElement = compDe.query(By.css('input')).nativeElement;
+    // set input's value to only match first option
+    inputEl.value = 'first';
+    // Dispatch a DOM event so that Angular learns of input value change.
+    inputEl.dispatchEvent(new Event('input'));
+    // Wait for debounceTime on inputFormControl
+    tick(QUARTER_SECOND);
+
+    // Using triggerEventHandler on inputDE would work here as well
+    inputEl.click();
+    
+    const firstOptionIndex = 0;
+    const secondOptionIndex = 1;
+    expect( compDe.query( By.css( `mat-option[qa-tag="type-ahead-option-${testOptions[firstOptionIndex].name}"]` ) ) ).toBeTruthy();
+    expect( compDe.query( By.css( `mat-option[qa-tag="type-ahead-option-${testOptions[secondOptionIndex].name}"]` ) ) ).toBeNull();
+  }));
 });
--- src/app/core-ui/ssa-type-ahead/ssa-type-ahead.component.ts
+++ src/app/core-ui/ssa-type-ahead/ssa-type-ahead.component.ts
@@ -1,15 +1,60 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatAutocompleteModule} from '@angular/material/autocomplete'; 
 import {MatInputModule} from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-ssa-type-ahead',
   standalone: true,
-  imports: [ MatFormFieldModule, MatAutocompleteModule, MatInputModule ],
+  imports: [
+    // Imports form field component
+    MatFormFieldModule,
+    // Imports autocomplete component
+    MatAutocompleteModule,
+    // Needed to set input as a matInput
+    MatInputModule,
+    // Needed to bind formControl to input
+    ReactiveFormsModule ],
   templateUrl: './ssa-type-ahead.component.html',
   styleUrl: './ssa-type-ahead.component.scss'
 })
-export class SsaTypeAheadComponent {
-  @Input() autoCompleteOptions = [];
+export class SsaTypeAheadComponent implements OnInit, OnDestroy {
+  private onDestroy$ = new Subject();
+  @Input() autoCompleteOptions: { name: string; value: string; }[] = [];
+
+  inputFormControl = new FormControl( '' );
+  optionsToDisplay: { name: string; value: string; }[] = [];
+
+  QUARTER_SECOND = 250;
+  TENTH_SECOND = 100;
+
+  ngOnInit(): void {
+    this.populateTypeAheadOptions();
+    this.listenToTypeAhead();
+  }
+  ngOnDestroy(): void {
+    this.onDestroy$.next( null );
+  }
+
+  populateTypeAheadOptions() {
+    this.optionsToDisplay = this.autoCompleteOptions;
+  }
+
+  listenToTypeAhead() {
+    this.inputFormControl.valueChanges
+    .pipe(
+      takeUntil(this.onDestroy$),
+      debounceTime(this.QUARTER_SECOND)
+    ).subscribe( ( data ) => {
+      if ( data ) {
+        this.optionsToDisplay = this.autoCompleteOptions.filter((option) => {
+          return option.name.toLocaleLowerCase().includes( data.toLocaleLowerCase() );
+        })
+      } else {
+        this.optionsToDisplay = this.autoCompleteOptions;
+      }
+    } );
+  }
 }
